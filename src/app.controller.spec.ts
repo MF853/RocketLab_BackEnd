904cb62e997bb41c62c380ae98f3f362217c693a
MF853 <mariofilho2003@gmail.com>
2025-06-02 23:46:06 -0300

    README Update

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -8,15 +7,14 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
     }).compile();
 
     appController = app.get<AppController>(AppController);
   });
 
-  describe('root', () => {
-    it('should return home page message', () => {
-      expect(appController.getHello()).toBe('Home Page Update!');
+  describe('AppController', () => {
+    it('should be defined', () => {
+      expect(appController).toBeDefined();
     });
   });
 });
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { CartModule } from './cart/cart.module';
@@ -9,6 +8,5 @@ import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [ProductModule, PrismaModule, CartModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
